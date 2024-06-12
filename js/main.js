a3cc048b281e3ec54b071d3b7375902cc6f7242c
chunzha <125763492+chunzha1@users.noreply.github.com>
2024-06-12 17:25:47 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -98,7 +98,12 @@ function init() {
         const materialGUI = gui.addFolder('Material Settings'); // 创建一个材质设置的折叠面板
         materialGUI.add(points.material, 'size', 0.001, 1.5).onChange(render); // 控制点的大小
         materialGUI.addColor(points.material, 'color').onChange(render); // 控制点的颜色
+        const backgroundColor = { color: '#000000' }; // 初始颜色为黑色
+        materialGUI.addColor(backgroundColor, 'color').onChange(function(value) {
+        renderer.setClearColor(new THREE.Color(value), 1);
+        });
         materialGUI.open(); // 默认展开材质设置的折叠面板
+
         //?
         render();
     });
