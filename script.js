ee92125cce66cf980f20ed09441f94e46278dded
chunzha <125763492+chunzha1@users.noreply.github.com>
2024-06-25 16:03:26 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+const startCallButton = document.getElementById('startCall');
+const callPeerButton = document.getElementById('callPeer');
+const peerIdInput = document.getElementById('peerId');
+
+let localStream;
+let peer;
+let call;
+
+startCallButton.addEventListener('click', async () => {
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    localVideo.srcObject = localStream;
+
+    peer = new Peer();
+
+    peer.on('open', (id) => {
+        console.log('My peer ID is: ' + id);
+    });
+
+    peer.on('call', (incomingCall) => {
+        incomingCall.answer(localStream);
+        incomingCall.on('stream', (remoteStream) => {
+            remoteVideo.srcObject = remoteStream;
+        });
+    });
+});
+
+callPeerButton.addEventListener('click', () => {
+    const peerId = peerIdInput.value;
+    if (peerId) {
+        call = peer.call(peerId, localStream);
+        call.on('stream', (remoteStream) => {
+            remoteVideo.srcObject = remoteStream;
+        });
+    }
+});
