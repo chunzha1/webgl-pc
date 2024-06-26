19edca483e7debf60bce95d11caaf000a54fa1fb
chunzha <125763492+chunzha1@users.noreply.github.com>
2024-06-26 11:27:33 +0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -15,49 +15,39 @@ peer.on('open', id => {
 
 peer.on('call', call => {
     statusDiv.innerText = 'Receiving a call...';
-    const mediaConstraints = {
-        video: enableVideoCheckbox.checked,
-        audio: enableAudioCheckbox.checked
-    };
-    navigator.mediaDevices.getUserMedia(mediaConstraints)
-        .then(stream => {
-            call.answer(stream);
-            localVideo.srcObject = stream;
-            call.on('stream', remoteStream => {
-                remoteVideo.srcObject = remoteStream;
-                statusDiv.innerText = 'Call connected!';
-            });
-        })
-        .catch(err => {
-            console.error('Failed to get local stream', err);
-            statusDiv.innerText = 'Failed to get local stream';
-        });
+    // const mediaConstraints = {
+    //     video: enableVideoCheckbox.checked,
+    //     audio: enableAudioCheckbox.checked
+    // };
+    // navigator.mediaDevices.getUserMedia(mediaConstraints)
+    //     .then(stream => {
+    //         call.answer(stream);
+    //         localVideo.srcObject = stream;
+    //         call.on('stream', remoteStream => {
+    //             remoteVideo.srcObject = remoteStream;
+    //             statusDiv.innerText = 'Call connected!';
+    //         });
+    //     })
+    //     .catch(err => {
+    //         console.error('Failed to get local stream', err);
+    //         statusDiv.innerText = 'Failed to get local stream';
+    //     });
 });
 
 callButton.addEventListener('click', () => {
     const peerId = peerIdInput.value;
     statusDiv.innerText = `Calling ${peerId}...`;
-    const mediaConstraints = {
-        video: enableVideoCheckbox.checked,
-        audio: enableAudioCheckbox.checked
-    };
-    navigator.mediaDevices.getUserMedia(mediaConstraints)
-        .then(stream => {
-            const call = peer.call(peerId, stream);
-            localVideo.srcObject = stream;
-            call.on('stream', remoteStream => {
-                remoteVideo.srcObject = remoteStream;
-                statusDiv.innerText = 'Call connected!';
-            });
-            call.on('error', err => {
-                console.error('Call error:', err);
-                statusDiv.innerText = `Call error: ${err}`;
-            });
-        })
-        .catch(err => {
-            console.error('Failed to get local stream', err);
-            statusDiv.innerText = 'Failed to get local stream';
-        });
+
+    const call = peer.call(peerId, null);
+    localVideo.srcObject = stream;
+    call.on('stream', remoteStream => {
+        remoteVideo.srcObject = remoteStream;
+        statusDiv.innerText = 'Call connected!';
+    });
+    call.on('error', err => {
+        console.error('Call error:', err);
+        statusDiv.innerText = `Call error: ${err}`;
+    });
 });
 
 peer.on('error', err => {
