29fb2234a8b07459b1e63d184675ba283699d59a
chunzha <125763492+chunzha1@users.noreply.github.com>
2024-07-17 10:18:13 +0800

    Update game-mobile.js

--- shot-game/game-mobile.js
+++ shot-game/game-mobile.js
@@ -6,7 +6,7 @@
 const renderer = new THREE.WebGLRenderer();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, -50, 0);
+camera.position.set(0, 0, -50);
 camera.lookAt(1, 0, 1);
 
 adjustWindowSize();
